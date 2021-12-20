780a5dd01694dd150da34c14866d1e9852944b32
anonymous anonymous <patrick@firstdraft.com>
2021-12-20 17:38:06 +0000

    Generate visits resource with ideas:scaffold

---
+++ app/controllers/visits_controller.rb
@@ -0,0 +1,58 @@
+class VisitsController < ApplicationController
+  before_action :set_visit, only: [:show, :edit, :update, :destroy]
+
+  # GET /visits
+  def index
+    @visits = Visit.all
+  end
+
+  # GET /visits/1
+  def show
+  end
+
+  # GET /visits/new
+  def new
+    @visit = Visit.new
+  end
+
+  # GET /visits/1/edit
+  def edit
+  end
+
+  # POST /visits
+  def create
+    @visit = Visit.new(visit_params)
+
+    if @visit.save
+      redirect_to @visit, notice: 'Visit was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /visits/1
+  def update
+    if @visit.update(visit_params)
+      redirect_to @visit, notice: 'Visit was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /visits/1
+  def destroy
+    @visit.destroy
+    redirect_to visits_url, notice: 'Visit was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_visit
+      @visit = Visit.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def visit_params
+      params.require(:visit).permit(:user_id, :location_id)
+    end
+end
