55750653b5591ec7b3da12799392878bebaa4bfe
anonymous anonymous <patrick@firstdraft.com>
2021-12-20 17:38:07 +0000

    Direct association between users and visits

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :visits,
+             :dependent => :destroy
+
   has_many   :pins,
              :dependent => :destroy
 
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,6 +1,8 @@
 class Visit < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
